5437d57e7cf41eab2602b224af13669178ad6b1c
Your Name <you@example.com>
2022-04-10 13:13:24 +0200

    socat

---
+++ vzlogger/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CONFIG_PATH=/data/options.json
+
+declare -x PORT=`jq -r '.PORT' $CONFIG_PATH`
+declare -x SERVER=`jq -r '.SERVER' $CONFIG_PATH`
+declare -x UART=`jq -r '.UART' $CONFIG_PATH`
+declare -x SOCAT=`jq -r '.SOCAT' $CONFIG_PATH`
+
+if [ "$SOCAT" = "true" ]; then
+	echo "starting: /usr/bin/socat pty,link=$UART,waitslave tcp:$SERVER:$PORT,reuseaddr"
+	sleep 10 && vzlogger --foreground -c /config/vzlogger_cfg/vzlogger.cfg & \
+	/usr/bin/socat pty,link=$UART,waitslave tcp:$SERVER:$PORT,reuseaddrusr
+	echo "finished"
+else
+	vzlogger --foreground -c /config/vzlogger_cfg/vzlogger.cfg
+fi
