341dd3c4f4c59a34b131cbea795c937183e348e8
Kast Bernd <kastbernd@gmx.de>
2022-09-18 16:52:44 +0200

    outrestart

--- vzlogger/run.sh
+++ vzlogger/run.sh
@@ -5,12 +5,15 @@ declare -x PORT=`jq -r '.PORT' $CONFIG_PATH`
 declare -x SERVER=`jq -r '.SERVER' $CONFIG_PATH`
 declare -x UART=`jq -r '.UART' $CONFIG_PATH`
 declare -x SOCAT=`jq -r '.SOCAT' $CONFIG_PATH`
-
-if [ "$SOCAT" = "true" ]; then
-	echo "starting: /usr/bin/socat pty,link=$UART,waitslave tcp:$SERVER:$PORT,reuseaddr"
-	sleep 10 && vzlogger --foreground -c /config/vzlogger_cfg/vzlogger.cfg & \
-	/usr/bin/socat -T 5 pty,link=$UART tcp:$SERVER:$PORT,reuseaddr
-	echo "finished"
-else
-	vzlogger --foreground -c /config/vzlogger_cfg/vzlogger.cfg
-fi
+while true
+do
+	if [ "$SOCAT" = "true" ]; then
+		echo "starting: /usr/bin/socat pty,link=$UART,waitslave tcp:$SERVER:$PORT,reuseaddr"
+		sleep 10 && vzlogger --foreground -c /config/vzlogger_cfg/vzlogger.cfg & \
+		/usr/bin/socat -T 5 pty,link=$UART tcp:$SERVER:$PORT,reuseaddr
+		echo "finished"
+	else
+		vzlogger --foreground -c /config/vzlogger_cfg/vzlogger.cfg
+	fi
+	sleep 1
+done
